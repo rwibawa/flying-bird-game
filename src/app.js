e0c90bed96dcabf9dc164c8502318a1f38e98b00
Ryan Wibawa <ryan.wibawa@gmail.com>
2023-04-22 20:57:28 -0700

    add component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+var myGamePiece;
+
 function startGame() {
   myGameArea.start();
+  myGamePiece = new component(30, 30, "blue", 10, 120);
 }
 
-var myGameArea = {
+const myGameArea = {
   canvas: document.createElement("canvas"),
 
   start: function() {
@@ -11,10 +14,28 @@ var myGameArea = {
     this.context = this.canvas.getContext("2d");
 
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.frameNo = 0;
+    this.interval = setInterval(updateGameArea, 20);;
   },
-  
+
   clear: function() {
     this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
   }
+}
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.update = function() {
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function updateGameArea() {
+  myGameArea.clear();
+  // myGamePiece.x += 1; // to check if it's drawn 50 times per second
+  myGamePiece.update();
 }
